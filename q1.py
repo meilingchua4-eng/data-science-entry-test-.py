2ce937062ff26a4e856d167e4ccca106af01332a
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-30 02:42:58 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,11 +1,11 @@
-x = int (10)  # x is a integer
-y = int (10)  # y is a integer
-z = int (10)  # z is a integer
+x = int (10)                    # x is a integer
+y = int (10)                    # y is a integer
+z = int (10)                    # z is a integer
 
-def swap ("x","y")
-  return "y" + " " + "x"
+def swap ("x","y")              # Invoke the function "swap"
+  return "y" + " " + "x"       
 
 If x > z and y > z :
-  print ("y", "x")
+  print ("y", "x")              # Both x and y are numeric
 Else :
-  print "z = -1"
+  print "z = -1"                # x and y is not numeric
