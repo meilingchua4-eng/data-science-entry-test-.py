cc706b4bb6dd4636efdd09116fb920977533ccbd
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-30 02:37:37 +0800

    Code q1.py
    To be compiled.

---
+++ q1.py
@@ -0,0 +1,11 @@
+x = int (10)  # x is a integer
+y = int (10)  # y is a integer
+z = int (10)  # z is a integer
+
+def swap ("x","y")
+  return "y" + " " + "x"
+
+If x > z and y > z :
+  print ("y", "x")
+Else :
+  print "z = -1"
