6d01203ee05cea0546569071d137b250f46e5aae
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-30 22:58:08 +0800

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,24 @@
+q3.py
+# Define variables
+
+name = float [ “Alice” , “Tom” , ”Bobby” , ”Irene” , “Charlene“, “Royston”]
+value = float [3]
+age = int [3]
+
+dct = list [name]
+key = list [value]
+new_age = list [age]
+
+# Call function
+def update_dictionary[“dct”, “key”, “value”]
+
+# Call dictionary
+update_dictionary[“dct”, “key”, “value”]
+
+	if update_dictionary(name = “dct”, value = “key” , new_age = “age”,]
+	  print (“name” , “key” , "new age”)
+
+	else :
+	  print (“name” , “key” , "new age”)
+
+Return
