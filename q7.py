9b8341bc3f14663a7df462035eae1692f81100e3
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 19:15:04 +0800

    Update q7.py

--- q7.py
+++ q7.py
@@ -27,43 +27,16 @@ def_str_(describe_car):
 
 # Call describe_car method ()
 describe_car = Car("car.name", "car.model", 4)
-print ("Make: " + "car.name", "Model: " + "car.model), "Year: " + 4)
+print ("Make: " + "car.name", "Model: " + "car.model), "Year: " + year(4))
  
   
   
     
- car =car_name("make", 50)
-car.
-            
+ 
 
   
-  def_str_(car):
-    return f"{car.model}({car.year}
-
-  
-  car1 = Car("  " ,
-    
-    , Model: Corolla, Year: 2020
-
-
-def describe_car(self):
-        print("Year Make Model" + "")
+ 
 
 
 
-
-class Car:
-    """
-    Task 1
-    - Define a class named Car with attributes: make, model, year
-    - Initialize these attributes in the __init__ method
-    - Add a method named describe_car() that prints information about the car as "Year Make Model"
-    """
-
-    def describe_car(self):
-        print("")
-
-
-# Task 2
-# Create an instance of the Car class with the following attributes and call describe_car method:
-# - Make: Toyota, Model: Corolla, Year: 2020
+      
