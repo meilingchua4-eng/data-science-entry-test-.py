0b3945b26d399d86b2cfa39c5fb5c569a5bc47f1
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 23:31:40 +0800

    Update q2.py

--- q2.py
+++ q2.py
@@ -4,12 +4,12 @@ mynumber1 = list(1 , 2 , 3, 4, 2, 2)
 mynumber2 = list(2 , 5)
 mynumber3 = list(x)
 
-# Define find_and_replace(mynumber1 , mynumber2) 
-def find_and_replace(mynumber1 , mynumber2) 
+# Define find_and_replace(mynumber1, mynumber2) 
+def find_and_replace(mynumber1, mynumber2) 
 	print("mynumber1" + "mynumber2")
 
-# Call find_and_replace(mynumber1 , mynumber2) 
-	find_and_replace(mynumber1 , mynumber2) 
+# Call find_and_replace(mynumber1, mynumber2) 
+	find_and_replace(mynumber1, mynumber2) 
 		if mynumber1 == mynumber2 :
 			print (“mynumber2”)
 	 	else :
