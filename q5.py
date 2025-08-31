3f43d8db6e2309a8ef18f1e5a0579688759c3656
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 18:00:50 +0800

    Create q5.py

---
+++ q5.py
@@ -0,0 +1,22 @@
+# define variables
+
+x = bool [5]
+num = int[20]
+divsor = int[20]
+remainder = float [20]
+
+#define function check_divisibility (num , divisor) 
+
+check_divisibility (num , divisor)  
+	print(“num” + “divisor + “remainder”)
+
+#Call Function check_divisibility (num , divisor)  
+
+check_divisibility (num,divisor)	
+
+	if remainder == 0 :
+		print(bool(“x” = “True”))
+	else if remainder <= 0 :
+		print(bool(“x” = “False”))
+	return
+
