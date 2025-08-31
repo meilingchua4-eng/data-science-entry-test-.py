51fea382981b625b36b1f2d81804045f7b02c2eb
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 18:03:31 +0800

    Create q6.py

---
+++ q6.py
@@ -0,0 +1,28 @@
+# Define variables
+i = int[15]
+x = int[10]
+y = int[10]
+
+x = list [1 , 1 , 2 , 1 , 2 , 1]
+y = list [1 , 2 , 1 , 1]
+lst1 = list [x]
+lst2 = list [y]
+lst = [“x” , ”y”]
+
+# Define function to find_first_negative(lst)
+def find_first_negative(lst) :
+	print(“The first negative number is” + “x” + “y”)
+
+# Use a while loop to find_first_negative(lst) in a list(lst)
+# Call function to find_first_negative(lst)
+
+find_first_negative(lst)
+
+	i = 1
+	while x < 10:
+	x += 1
+	if x >= 0:
+		continue
+		print (“x” + "No negatives")
+ 
+ return	
