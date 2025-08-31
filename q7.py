787323adce5260ab4e3a15e9c50400231f333f6e
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 19:08:04 +0800

    Create q7.py

---
+++ q7.py
@@ -0,0 +1,69 @@
+# Define variables
+x = str[15]
+y = str[100]
+z = int[4]
+
+# Define class Car
+class Car:
+make = x
+model = y
+year = z
+
+#Call Class Car
+class Car:
+  def_init_(make, model, year):
+    car.name = make
+    car.model = model
+    car.year = year
+
+# Add method named describe_car() that prints information about the car as "Year Make Model"
+describe_car = Car("car.name", "car.model", 4)
+print(describe_car.make)
+print(describe_car.model)
+print(describe_car.year)
+
+def_str_(describe_car):
+     print (car.model + ":=" + "Toyota", car.year + ":=" + "year")
+
+# Call describe_car method ()
+describe_car = Car("car.name", "car.model", 4)
+print ("Make: " + "car.name", "Model: " + "car.model), "Year: " + 4)
+ 
+  
+  
+    
+ car =car_name("make", 50)
+car.
+            
+
+  
+  def_str_(car):
+    return f"{car.model}({car.year}
+
+  
+  car1 = Car("  " ,
+    
+    , Model: Corolla, Year: 2020
+
+
+def describe_car(self):
+        print("Year Make Model" + "")
+
+
+
+
+class Car:
+    """
+    Task 1
+    - Define a class named Car with attributes: make, model, year
+    - Initialize these attributes in the __init__ method
+    - Add a method named describe_car() that prints information about the car as "Year Make Model"
+    """
+
+    def describe_car(self):
+        print("")
+
+
+# Task 2
+# Create an instance of the Car class with the following attributes and call describe_car method:
+# - Make: Toyota, Model: Corolla, Year: 2020
