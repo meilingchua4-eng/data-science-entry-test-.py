bb72aa3cd347c2f6f527a5b3feac587a5871f50d
meilingchua4-eng <meilingchua4@gmail.com>
2025-08-31 22:59:03 +0800

    Update q1.py

--- q1.py
+++ q1.py
@@ -1,11 +1,14 @@
-x = int (10)                    # x is a integer
-y = int (10)                    # y is a integer
-z = int (10)                    # z is a integer
+                                                          # Define variables
+x = int (20)                                              # x is the first numeric number
+y = int (20)                                              # y is the second numeric number
+z = str (2)                 
 
-def swap ("x","y")              # Invoke the function "swap"
-  return "y" + " " + "x"       
+def swap ("x","y")                                        # Define swap(x, y)
+  print("y" + "," + "x")       
 
-If x > z and y > z :
-  print ("y", "x")              # Both x and y are numeric
-Else :
-  print "z = -1"                # x and y is not numeric
+swap ("x","y")                                            # Call swap(x, y)
+if x > z and y > z :
+  print ("x and y is not numeric")            # Both x and y are numeric
+else :
+  print ("x and y is numeric")            # x and y is not numeric
+return
